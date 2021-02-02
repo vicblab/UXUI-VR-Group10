1890788a88fd3ca5eef1a58cde5dd659f9f7c587
aNaykki <alarik.naykki@edu.turkuamk.fi>
2021-02-02 23:20:28 +0200

    Ball with trail by speed

---
+++ VRBowling/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    private Material mat;
+    private TrailRenderer trail;
+    private Rigidbody rb;
+    [SerializeField] Gradient gradient;
+
+    private Vector3 ogPos;
+
+    private void Start()
+    {
+        ogPos = transform.position;
+        mat = GetComponent<Renderer>().material;
+        trail = GetComponent<TrailRenderer>();
+        rb = GetComponent<Rigidbody>();
+        SetColors(0f);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+            transform.position = ogPos;
+    }
+    private void FixedUpdate()
+    {
+        SetColors(rb.velocity.magnitude / 20f);
+    }
+    private void SetColors(float value)
+    {
+        Color color = gradient.Evaluate(value);
+        mat.SetColor("_OtlColor", color);
+        trail.material.color = color;
+
+    }
+}
