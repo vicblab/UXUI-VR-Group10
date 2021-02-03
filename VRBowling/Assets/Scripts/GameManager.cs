00e62a88343c499c53ae82e81fdc08a203482321
Víctor White <36444816+vicblab1@users.noreply.github.com>
2021-02-03 22:49:40 +0100

    Basic Game Manager, pin prefab and basic game logic dded

---
+++ VRBowling/Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+
+    
+    public static GameManager theGameManager;
+    
+    [SerializeField] private int totalPins = 10;
+    [SerializeField] private int fallenPins = 0;
+    [SerializeField] private int totalBalls = 5;
+    [SerializeField] private int playedBalls = 0;
+    //[SerializeField] private int currentGame = 0; // 0, 1 or 2
+    //[SerializeField] private int score0 = 0;
+    //[SerializeField] private int score1 = 0;
+    //[SerializeField] private int score2 = 0;
+    [SerializeField] private int score = 0;
+
+    
+
+
+    //Events
+    public UnityEvent strikeEvent = new UnityEvent();
+    public UnityEvent spareEvent = new UnityEvent();
+    public UnityEvent pinHasFallen = new UnityEvent();
+
+
+    public int TotalBalls { get => totalBalls; set => totalBalls = value; }
+    public int PlayedBalls { get => playedBalls; set => playedBalls = value; }
+    public int TotalPins { get => totalPins; set => totalPins = value; }
+    public int FallenPins { get => fallenPins; set => fallenPins = value; }
+    public int Score { get => score; set => score = value; }
+
+
+// We make sure that there is only one instance of the Game Manager
+
+    void Awake() {
+        if (!theGameManager) {
+            theGameManager = this;
+            return;
+        }
+
+        Destroy(this.gameObject);
+        
+    }
+
+    void Start()
+    {
+        //initialization
+        totalPins = 10;
+        fallenPins = 0;
+        totalBalls = 5;
+        playedBalls = 0;
+        //currentGame = 0; // 0, 1 or 2
+        //score0 = 0;
+        //score1 = 0;
+        //score2 = 0;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Restart() {
+        this.Start();
+        
+    }
+
+
+    public void fallenPin(){
+        fallenPins++;
+        score++;
+    }
+
+}
---
+++ VRBowling/Assets/Scripts/Pin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    private bool hasFallen=false;
+    private GameManager gameManager;
+
+
+    void Start(){
+
+       gameManager = FindObjectOfType<GameManager>();
+
+    }
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Floor"))
+        {
+            if(!hasFallen){
+            gameManager.fallenPin();
+            hasFallen=true;
+            }
+        }
+
+    }
+}
