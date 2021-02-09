5f7660e7512146b99fa3949fa0d5d6d0c484eb68
Víctor White <36444816+vicblab1@users.noreply.github.com>
2021-02-09 23:11:55 +0200

    Music added when slider door opens (the doorhandle enters an invisible trigger)

---
+++ VRBowling/Assets/sounds/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    AudioSource audioData;
+    bool hasBeenPlayed=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      audioData = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other){
+
+        if(!hasBeenPlayed){
+		audioData.Play();
+
+		hasBeenPlayed=true;
+	}
+    }
+}
